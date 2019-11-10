f6ab4391c26f5a6e60ec7a96bc0c7b3d86db0148
Aleksa <lexamatovic@gmail.com>
2019-11-10 23:32:01 +0100

    controller for user

--- flightreservation/src/main/java/com/matovic/flightreservation/controller/UserController.java
+++ flightreservation/src/main/java/com/matovic/flightreservation/controller/UserController.java
@@ -2,8 +2,11 @@ package com.matovic.flightreservation.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.matovic.flightreservation.entities.User;
 import com.matovic.flightreservation.repos.UserRepository;
 
 @Controller
@@ -15,5 +18,12 @@ public class UserController {
 	@RequestMapping("/showReg")
 	public String showRegistrationPage() {
 		return "login/registerUser";
+	}
+	
+	
+	@PostMapping("/registerUser")
+	public String registerUser(@ModelAttribute("user") User user) {
+		userRepository.save(user);
+		return "login/login";
 	}
 }
