cb953abbb9a08425f3d535a4960a5c571a8b0e9c
Aleksa <lexamatovic@gmail.com>
2019-11-17 11:43:18 +0100

    implement query for findByEmail

--- flightreservation/src/main/java/com/matovic/flightreservation/repos/UserRepository.java
+++ flightreservation/src/main/java/com/matovic/flightreservation/repos/UserRepository.java
@@ -1,11 +1,14 @@
 package com.matovic.flightreservation.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.matovic.flightreservation.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-	User findByEmail(String email);
+	@Query("from User where email=:EMAIL and password=:PASS")
+	User findByEmail(@Param("EMAIL") String email, @Param("PASS") String password);
 
 }
