e2ea4e85fe99e3c6fdc6631d0feaaec3896ddfda
lexam <lexam@LAPTOP-BPRBOSE2>
2019-12-05 23:10:21 +0100

    dodat crossOrigin za komunikaciju izmednju aplikacija

--- flightreservation/src/main/java/com/matovic/flightreservation/controller/ReservationRestController.java
+++ flightreservation/src/main/java/com/matovic/flightreservation/controller/ReservationRestController.java
@@ -1,6 +1,7 @@
 package com.matovic.flightreservation.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import com.matovic.flightreservation.entities.Reservation;
 import com.matovic.flightreservation.repos.ReservationRepository;
 
 @RestController
+@CrossOrigin  // neophodno za komunikaciju sa angularom (tj generalno medjusobnu komunikaciju)
 public class ReservationRestController {
 
 	@Autowired
