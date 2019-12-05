da56a57528d7fffe76957b6721ce3b6a7f20550c
lexam <lexam@LAPTOP-BPRBOSE2>
2019-12-05 23:06:50 +0100

    added update reservations (checking in)

--- flightreservation/src/main/java/com/matovic/flightreservation/controller/ReservationRestController.java
+++ flightreservation/src/main/java/com/matovic/flightreservation/controller/ReservationRestController.java
@@ -1,11 +1,12 @@
 package com.matovic.flightreservation.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.matovic.flightreservation.dto.ReservationUpdateRequest;
 import com.matovic.flightreservation.entities.Reservation;
 import com.matovic.flightreservation.repos.ReservationRepository;
 
@@ -18,5 +19,15 @@ public class ReservationRestController {
 	@RequestMapping("/reservations/{id}")
 	public Reservation findReservation(@PathVariable("id") Long id) {
 		return reservationRepository.findById(id).get();
+	}
+	
+	@RequestMapping("/reservations")
+	public Reservation updateReservation(@RequestBody ReservationUpdateRequest request) {
+		
+		Reservation reservation = reservationRepository.findById(request.getId()).get(); 
+		reservation.setNumberOfBags(request.getNumberOfBags());
+		reservation.setCheckedIn(request.isCheckedIn());
+		
+		return reservationRepository.save(reservation);
 	}
 }
